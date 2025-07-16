8da99eba0ded1cb2a5b5a685172405c35d0f8b28
zavokair <cyberbeasterror@gmail.com>
2025-07-16 16:35:02 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,1 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', paddingTop: '50px' }}>
+      <h1>Welcome to NeonFans 🔥</h1>
+      <p>Your new OnlyFans-style platform is live!</p>
+    </div>
+  );
+}
+
+export default App;
 // React App.js placeholder
